e9f078f37ebedb96028181185224af4caf301713
zecst19 <zecst19@gmail.com>
2015-12-20 15:52:12 +0000

    Update Oficina.h

--- Código/Oficina.h
+++ Código/Oficina.h
@@ -32,25 +32,6 @@ public:
 
 };
 
-struct oficinaHash
-{
-	int operator() (const Oficina& o1)
-	{
-		int v = 0;
-		for(unsigned int i = 0; i < o1.getDenominacao().size(); i++)
-		{
-			v = v*37 + o1.getDenominacao()[i];
-		}
-		return v;
-	}
 
-	bool operator() (const Oficina& o1, const Oficina& o2)
-	{
-		if(o1.getDenominacao() == o2.getDenominacao())
-			return true;
-		else
-			return false;
-	}
-};
 
 #endif /* SRC_OFICINA_H_ */
